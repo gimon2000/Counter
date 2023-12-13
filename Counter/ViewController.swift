9272bc602c8bb8dc24b8637864f15f7107737a33
Gimon Anton <Gimon2000@yandex.ru>
2023-12-14 00:09:18 +0300

    corrections after review

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -12,7 +12,10 @@ class ViewController: UIViewController {
     private var count = 0
     private var increment = 1
     private let date = Date()
-    private let calendar = Calendar.current
+    
+    @IBOutlet private weak var counterLabel: UILabel!
+    @IBOutlet private weak var history: UITextView!
+    
     private func getDate() -> String {
         let date = Date()
         let df = DateFormatter()
@@ -20,49 +23,49 @@ class ViewController: UIViewController {
         return df.string(from: date)
     }
     
+    private func addTextToHistory(_ text: String){
+        history.text += "\(getDate()): \(text)"
+        let range = NSRange(location: history.text.count - 1, length: 0)
+        history.scrollRangeToVisible(range)
+    }
+    
     private func getCountText(_ number: Int) -> String {
         "Значение счётчика: \(number)"
     }
-
-    @IBOutlet weak var counterLabel: UILabel!
-    @IBOutlet weak var increaseCounterButton: UIButton!
-    
-    @IBOutlet weak var piusButton: UIButton!
-    @IBOutlet weak var minusButton: UIButton!
-    @IBOutlet weak var resetButton: UIButton!
-    @IBOutlet weak var history: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         counterLabel.text = getCountText(count)
         history.text = "История изменений:\n"
     }
-
-    @IBAction func clickCounterButton(_ sender: Any) {
-        if increment == -1, count == 0 {
-            history.text += "\(getDate()): попытка уменьшить значение счётчика ниже 0\n"
-            return
+    
+    @IBAction private func clickPlusButton(_ sender: Any) {
+        if increment == -1 {
+            increment = 1
+            addTextToHistory("значение изменено на +1\n")
         }
         count += increment
         counterLabel.text = getCountText(count)
     }
     
-    @IBAction func clickPlusButton(_ sender: Any) {
-        increment = 1
-        history.text += "\(getDate()): значение изменено на +1\n"
+    @IBAction private func clickMinusButton(_ sender: Any) {
+        if count == 0 {
+            addTextToHistory("попытка уменьшить значение счётчика ниже 0\n")
+            return
+        }
+        if increment == 1 {
+            increment = -1
+            addTextToHistory("значение изменено на -1\n")
+        }
+        count += increment
+        counterLabel.text = getCountText(count)
     }
     
-    @IBAction func clickMinusButton(_ sender: Any) {
-        increment = -1
-        history.text += "\(getDate()): значение изменено на -1\n"
-    }
-    
-    @IBAction func clickResetButton(_ sender: Any) {
+    @IBAction private func clickResetButton(_ sender: Any) {
         count = 0
         increment = 1
         counterLabel.text = getCountText(count)
-        history.text += "\(getDate()): значение сброшено\n"
+        addTextToHistory("значение сброшено\n")
     }
     
 }
